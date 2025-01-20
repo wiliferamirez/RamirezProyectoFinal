79a28a679e1051c18ec0ad997ea11038aa89c4f6
William Fernando Ramirez Paucar <william.ramirez.paucar@udla.edu.ec>
2025-01-19 21:49:27 -0500

    Add mvc structure and repository for external api

--- RamirezforaneoAppMaui/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/RamirezforaneoAppMaui.AssemblyInfo.cs
+++ RamirezforaneoAppMaui/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/RamirezforaneoAppMaui.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RamirezforaneoAppMaui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+49b0598c2fd2fdac7d8717bdd7eb25bc33ca731d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0ac233f8d379758b0390b86131d554a2074fa58d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RamirezforaneoAppMaui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RamirezforaneoAppMaui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
