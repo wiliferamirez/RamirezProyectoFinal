de8ebd07dfab9e0fe2c2a2cf412aca694f043f96
William Fernando Ramirez Paucar <william.ramirez.paucar@udla.edu.ec>
2025-01-22 20:15:20 -0500

    Create Login VIew

---
+++ RamirezforaneoAppMaui/Views/Admin/Login.xaml.cs
@@ -0,0 +1,19 @@
+namespace RamirezforaneoAppMaui.Views.Admin;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+
+    private void OnLoginClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void OnRegisterClicked(object sender, EventArgs e)
+    {
+
+    }
+}
