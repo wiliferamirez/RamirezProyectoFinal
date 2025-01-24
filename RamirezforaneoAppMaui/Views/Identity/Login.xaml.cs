57f892a2eff4e2d5deb9b2dfd0a0fce18ec2011a
William Fernando Ramirez Paucar <william.ramirez.paucar@udla.edu.ec>
2025-01-24 17:43:40 -0500

    Add login and register actions to Login.xaml

--- RamirezforaneoAppMaui/Views/Identity/Login.xaml.cs
+++ RamirezforaneoAppMaui/Views/Identity/Login.xaml.cs
@@ -6,4 +6,14 @@ public partial class Login : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void OnLoginClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void OnRegisterClicked(object sender, EventArgs e)
+    {
+
+    }
 }
