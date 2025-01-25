bfc772f77738e9c4b1ff7511832ac1c580a43e22
William Fernando Ramirez Paucar <william.ramirez.paucar@udla.edu.ec>
2025-01-25 15:23:04 -0500

    Add Register method to Authentication Service

--- RamirezforaneoAppMaui/Services/AuthenticationService.cs
+++ RamirezforaneoAppMaui/Services/AuthenticationService.cs
@@ -24,7 +24,23 @@ namespace RamirezforaneoAppMaui.Services
             return await response.Content.ReadFromJsonAsync<LoginResponse>();
 
         }
+        public async Task<bool> RegisterAsync(RegisterRequest registerModel)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/UserManagement/register", registerModel);
+            return response.IsSuccessStatusCode;
+        }
     }
+
+    public class RegisterRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string CedulaUser { get; set; }
+        public string StateName { get; set; }
+        public string StudyProgram { get; set; }
+        public int SessionNumber { get; set; }
+    }
+
     public class LoginResponse
     {
         public string UserId { get; set; }
