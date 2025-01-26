054579767f14cba7224cc45e0df8d4fd6c7865ed
William Fernando Ramirez Paucar <william.ramirez.paucar@udla.edu.ec>
2025-01-26 00:42:20 -0500

    Add method to retrieve all categories list

--- RamirezforaneoAppMaui/Services/CategoriesService.cs
+++ RamirezforaneoAppMaui/Services/CategoriesService.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net.Http.Json;
+using RamirezforaneoAppMaui.Models.Admin;
+
 
 namespace RamirezforaneoAppMaui.Services
 {
     public class CategoriesService
     {
+        private readonly HttpClient _httpClient;
+
+        public CategoriesService()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://rh4p8xrf-5262.use2.devtunnels.ms/");
+        }
+
+        public async Task<List<Category>> GetCategoriesAsync()
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync("api/CategoriesManagement");
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadFromJsonAsync<List<Category>>();
+                }
+                else
+                {
+                    
+                    return new List<Category>();
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                return new List<Category>();
+            }
+        }
+
     }
 }
