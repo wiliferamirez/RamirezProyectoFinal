80fa65b280952f9c8ae7dd15667bbe1372b28709
William Fernando Ramirez Paucar <william.ramirez.paucar@udla.edu.ec>
2025-01-26 00:54:52 -0500

    Modify view to display data retrieved from API

--- RamirezforaneoAppMaui/Views/Admin/CategoriesManagement/Index.xaml.cs
+++ RamirezforaneoAppMaui/Views/Admin/CategoriesManagement/Index.xaml.cs
@@ -1,3 +1,4 @@
+using RamirezforaneoAppMaui.ViewModel.Admin;
 namespace RamirezforaneoAppMaui.Views.Admin.CategoriesManagement;
 
 public partial class Index : ContentPage
@@ -5,5 +6,6 @@ public partial class Index : ContentPage
 	public Index()
 	{
 		InitializeComponent();
-	}
+        BindingContext = new CategoriesViewModel();
+    }
 }
