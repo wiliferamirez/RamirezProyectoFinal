5f877386cd1d5320c34ab8d8f7c3d44b26049a8d
William Fernando Ramirez Paucar <william.ramirez.paucar@udla.edu.ec>
2025-01-26 00:49:48 -0500

    Handle retrieved data from CategoriesView model

---
+++ RamirezforaneoAppMaui/ViewModel/Admin/CategoriesViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using RamirezforaneoAppMaui.Models.Admin;
+using RamirezforaneoAppMaui.Services;
+using CommunityToolkit.Mvvm.Input;
+
+namespace RamirezforaneoAppMaui.ViewModel.Admin
+{
+    public partial class CategoriesViewModel : ObservableObject
+    {
+        private readonly CategoriesService _categoriesService;
+
+        [ObservableProperty]
+        private ObservableCollection<Category> categories;
+
+        public CategoriesViewModel()
+        {
+            _categoriesService = new CategoriesService();
+        }
+
+        [RelayCommand]
+        public async Task LoadCategoriesAsync()
+        {
+            var categoriesList = await _categoriesService.GetCategoriesAsync();
+            categories = new ObservableCollection<Category>(categoriesList);
+        }
+    }
+}
